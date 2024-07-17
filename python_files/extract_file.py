4785043dfb0732f51ae673261aa5a9e6782f2698
Ikanga93 <153716887+Ikanga93@users.noreply.github.com>
2024-07-16 20:17:56 -0400

    commit

--- python_files/extract_file.py
+++ python_files/extract_file.py
@@ -2,6 +2,10 @@
 from sodapy import Socrata
 from config import load_config
 from datetime import datetime
+import logging
+
+# Logging configuration
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 # Config file path
 config = load_config("/Users/jbshome/Desktop/tlc_application_etl/configuration/config.json")
@@ -14,20 +18,25 @@ def get_last_extraction_time():
     try:
         with open("last_extraction.txt", "r") as file:
             for line in file:
-                if not line.startwith("#"):
-                    last_extracttion = file.read().strip()
-                    return datetime.fromisoformat(last_extracttion)
+                if not line.startswith("#"):
+                    last_extraction = file.read().strip()
+                    # return datetime.fromisoformat(last_extraction)
         # If no valid timestamp found, return default
         return DEFAULT_TIMESTAMP
     except FileNotFoundError:
         # Default value if file not found
         return DEFAULT_TIMESTAMP
+    
+get_last_extraction_time()
 
-# # Function to save the current extraction timestamp to a file
+# Function to save the current extraction timestamp to a file
 def save_current_extraction_time():
     with open("last_extraction.txt", "w") as file:
         file.write("# Last extraction timestamp\n")
         file.write(datetime.now().isoformat() + "\n")
+        return file
+    
+save_current_extraction_time()
 
 # Function to extract data
 def extract(*args):
@@ -39,4 +48,4 @@ def extract(*args):
 
 if __name__ == "__main__":
     extracted_data = extract(config["tlc_api_url"], config["tlc_app_token"], config["tlc_username"], config["tlc_password"])
-    # print(extracted_data)
+    #print(extracted_data)
